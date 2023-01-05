d243c3262b03b3118a0de0c141bb029894c8b2e5
BOMZ <adsadawutbomz.23@gmail.com>
2023-01-05 21:35:17 +0700

    first commit

---
+++ src/main/java/dev/childer/childerserver/ChilderServerApplication.java
@@ -0,0 +1,13 @@
+package dev.childer.childerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChilderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChilderServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/childer/childerserver/ChilderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.childer.childerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChilderServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
